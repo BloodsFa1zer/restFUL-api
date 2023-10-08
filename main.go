d89f21c7e99029b4e2ccf85c92f45600f9d88b2a
BloodsFa1zer <mixo228aga@gmail.com>
2023-10-08 23:57:12 +0300

    added connection to the redisDB

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"app3.1/Middleware"
-	"app3.1/routes"
-	"github.com/labstack/echo/v4"
+	"app3.1/database"
+	"fmt"
+	"github.com/go-redis/redis"
 )
 
 func main() {
-	e := echo.New()
-	e.HidePort = true
+	//e := echo.New()
+	//e.HidePort = true
+	//
+	//Middleware.UserAuth(e)
+	//routes.UserRoute(e)
+	//
+	//e.Logger.Fatal(e.Start(":6000"))
 
-	Middleware.UserAuth(e)
-	routes.UserRoute(e)
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 
-	e.Logger.Fatal(e.Start(":6000"))
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	// we can call set with a Key' and a 'Value'
+	err = client.Set("1", database.User{}, 0).Err()
+	// if there has been an error setting the value
+	// handle the error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	val, err := client.Get("name").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(val)
 }
